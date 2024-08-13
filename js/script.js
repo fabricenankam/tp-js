8ab7b54add987fdd3fda41f279a9cbc6d6e6c0c4
Fabrice Nankam <fanas009@yahoo.fr>
2024-08-13 20:21:39 +0200

    modification fichier js

--- js/script.js
+++ js/script.js
@@ -83,6 +83,120 @@ const addition = (a,b) => {
 
 addition(x,y);
 
+//les classes 
+class Rectangle {
+
+    constructor(width, height){
+
+    //les erreurs
+        if(width < 0 || height < 0){
+            throw new Error("impossible d'avoir un rectangle avec une dimension négative !");
+        }
+    //fin code erreur
+
+        this.width=width;
+        this.height=height;
+    }
+
+    get perimeter(){
+        return (this.width + this.height)*2;
+    }
+
+    get isValid(){
+        return this.width > 0 && this.height > 0;
+    }
+
+    isBiggerThan(shape){
+        return this.perimeter > shape.perimeter;
+    }
+
+}
+
+class Square extends Rectangle{
+
+    constructor(width){
+        super(width,width);
+    }
+}
+
+
+
+let r1 = new Rectangle(10,20);
+console.log(r1);
+
+
+console.log(r1.perimeter);
+console.log(r1.isValid);
+
+    //les erreurs
+try{
+    let r2 = new Rectangle(-10,18);
+    console.log(r2.perimeter);
+    console.log(r2.isValid);
+}catch(e){
+console.log(e.message);
+}
+    //fin code erreur
+
+let s = new Square(20,20);
+console.log(s.perimeter);
+console.log(s.isValid);
+
+console.log(r1.isBiggerThan(s));
+
+//les erreurs
+
+try{
+    let width = parseInt(prompt("Largeur"), 10);
+    let height = parseInt(prompt("hauteur"), 10);
+    let r = new Rectangle(width,height);
+    console.log(r.perimeter);    
+}catch(e){
+console.log("impossible de construire le rectangle");
+}
+
+//les fonctions usuelles
+   //les tableaux
+
+   let tab = [5, 25, 17, 63, 38, 56];
+   console.log(tab.at(-2));
+
+   let goodNotes = tab.filter((note) => (note >= 50));
+   console.log(goodNotes);
+
+   tab.forEach((notes, index) => {
+    console.log(notes, index)
+   })
+   
+   let personnes = [
+    {firstname: "Eric" , lastname: "kamdem"},
+    {firstname: "jean" , lastname: "mbarga"},
+    {firstname: "serges" , lastname: "edzoa"},
+    {firstname: "bertrand" , lastname: "essomba"},
+]
+
+console.log(
+    personnes
+    .map((p) => p.firstname+" "+p.lastname)
+    .join("\n")
+);
+
+     //les objets
+
+     
+
+   
+
+   
+   
+
+   
+
+
+
+
+
+
 
 
 
